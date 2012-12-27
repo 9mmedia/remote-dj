c42dd7474af4e2496fb2e02396c372170929a384
Joe Morgan <joe@9mmedia.com>
2012-12-27 14:44:11 -0500

    Committing Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemoteDj::Application.config.secret_token = '51575d6d8621d21ac4245bb61cfdd1a3525420fdd536ae97f3bc92febfff921b3e9b3b27af89a69a4b1c9afdb17cdbfb8ce96dd6e8310b8fedb1b9703e61faa0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RemoteDj::Application.config.session_store :cookie_store, key: '_remote-dj_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RemoteDj::Application.config.session_store :active_record_store
