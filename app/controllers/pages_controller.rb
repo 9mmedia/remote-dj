0196c3e84cc70e84d9128eefa23289e86ff034e6
Javier Julio <jjfutbol@gmail.com>
2012-12-28 16:43:58 -0500

    Add working index and queue actions loading all necessary data.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
 
   def index
-    
+    @playlist = Models::Playlist.default
+    @current_track = @playlist.current_track
+    next_start_index = @playlist.current_track_index + 1
+    @next_tracks = @playlist.tracks.slice(next_start_index, [@playlist.tracks.length, 10].min)
+  end
+
+  def queue
+    track = Models::Track.new params
+    playlist = Models::Playlist.default
+    playlist.add_track track
+    render nothing: true, status: 200, content_type: 'text/html'
   end
 
   def now_playing
