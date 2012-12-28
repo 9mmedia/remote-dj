3860bd45b3b81ed70fcae16779d6317fa27409ef
Javier Julio <jjfutbol@gmail.com>
2012-12-27 20:08:59 -0500

    Update show action to now search tracks using Spotify with q param provided.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,9 @@
 class SearchesController < ApplicationController
 
   def show
-    
+    @query = params[:q]
+    spotify = Services::Spotify.new
+    @results = spotify.search_tracks @query
     render layout: false
   end
 
